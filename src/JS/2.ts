fd08659b03f76384f4325c6fde0987c0f424669d
omykhalska <omykhalska@gmail.com>
2023-10-10 15:21:55 +0200

    add task 2

---
+++ src/JS/2.ts
@@ -0,0 +1,12 @@
+function* chunkArray(arr: Array<number>, num: number) {
+  while (arr.length > num) {
+    yield arr.splice(0, num);
+  }
+  yield arr;
+}
+
+const iterator = chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3);
+console.log(iterator.next()); // { value: [1, 2, 3], done: false }
+console.log(iterator.next()); // { value: [4, 5, 6], done: false }
+console.log(iterator.next()); // { value: [7, 8], done: false }
+console.log(iterator.next()); // { value: undefined, done: true }
